4a753c648d68651a95bccc78845e961d2bcaabf9
ao1415 <ao14159265@gmail.com>
2020-11-28 20:46:37 +0900

    乱数を整備

--- FallChallenge2020/main.cpp
+++ FallChallenge2020/main.cpp
@@ -291,15 +291,70 @@ public:
 	}
 };
 
-/**
- * @brief xoshiro128++
- *
- */
-class XoShiro128
+class Random
 {
 public:
 	using value_type = uint32_t;
 
+	/**
+	 * @brief 32bitの乱数値を取得する
+	 *
+	 * @return value_type
+	 */
+	[[nodiscard]] inline virtual value_type next() = 0;
+
+	/**
+	 * @brief [0.0, 1.0)の範囲の乱数値を取得する
+	 *
+	 * @return float [0.0, 1.0)
+	 */
+	[[nodiscard]] inline float nextFloat()
+	{
+		const value_type v = (next() >> 9) | 0b0'01111111'00000000000000000000000;
+
+		return (*((float *)(&v))) - 1.0f;
+	}
+	/**
+	 * @brief [0.0, 1.0)の範囲の乱数値を取得する
+	 *
+	 * @return double [0.0, 1.0)
+	 */
+	[[nodiscard]] inline double nextDouble()
+	{
+		const uint64_t v = (static_cast<uint64_t>(next()) << (32 - 12)) | 0b0'01111111111'0000000000000000000000000000000000000000000000000000ull;
+
+		return (*((double *)(&v))) - 1.0;
+	}
+};
+
+class XorShift : public Random
+{
+private:
+	value_type m_state = 2463534242u;
+	
+public:
+
+	/**
+	 * @brief 32bitの乱数値を取得する
+	 *
+	 * @return value_type
+	 */
+	[[nodiscard]] inline value_type next()
+	{
+		m_state ^= (m_state << 13);
+		m_state ^= (m_state >> 17);
+		m_state ^= (m_state << 5);
+		
+		return m_state;
+	}
+};
+
+/**
+ * @brief xoshiro128++
+ *
+ */
+class XoShiro128 : public Random
+{
 private:
 	value_type m_state0 = 723471715u;
 	value_type m_state1 = 2497366906u;
@@ -307,14 +362,13 @@ private:
 	value_type m_state3 = 2008045182u;
 
 public:
-	XoShiro128() {}
 
 	/**
 	 * @brief 32bitの乱数値を取得する
 	 *
 	 * @return value_type
 	 */
-	[[nodiscard]] value_type next()
+	[[nodiscard]] inline value_type next()
 	{
 		const value_type rotl_tmp = m_state0 + m_state3;
 		const value_type result = ((rotl_tmp << 7) | (rotl_tmp >> (32 - 7))) + m_state0;
@@ -331,31 +385,6 @@ public:
 		m_state3 = ((m_state3 << 11) | (m_state3 >> (32 - 11)));
 
 		return result;
-	}
-
-	/**
-	 * @brief [0.0, 1.0)の範囲の乱数値を取得する
-	 *
-	 * @return float [0.0, 1.0)
-	 */
-	[[nodiscard]] float nextFloat()
-	{
-		const value_type v = next();
-
-		return static_cast<float>((v >> 8) * 0x1.0p-24f);
-	}
-	/**
-	 * @brief [0.0, 1.0)の範囲の乱数値を取得する
-	 *
-	 * @return double [0.0, 1.0)
-	 */
-	[[nodiscard]] double nextDouble()
-	{
-		const value_type v1 = next();
-		const value_type v2 = next();
-		const uint64_t v = (static_cast<uint64_t>(v1) << 32) | static_cast<uint64_t>(v2);
-
-		return static_cast<double>((v >> 11) * 0x1.0p-53);
 	}
 };
 
